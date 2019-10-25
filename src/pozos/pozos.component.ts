6dc9d52fce25ebf1e71f319fb0420e6570fd37ca
JohanRivera <johan.rivera19@hotmail.com>
2019-10-25 09:04:15 -0500

    3 files modified, 2 added

---
+++ src/pozos/pozos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pozos',
+  templateUrl: './pozos.component.html',
+  styleUrls: ['./pozos.component.css']
+})
+export class PozosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
